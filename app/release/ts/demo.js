b6470bd49a883737042f941119ea46e4f5ce0144
Paulo andrade <source.compu@gmail.com>
2016-07-20 01:12:50 -0500

    Subiendo archivos del proyecto

---
+++ app/release/ts/demo.js
@@ -0,0 +1,17 @@
+// Ejemplo tomado de la web principal de typescript https://typescriptlang.org 
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+var button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function () {
+    alert(greeter.greet());
+};
+document.body.appendChild(button);
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var concat = require('gulp-concat'), // Concatena archivos
+	gulp = require('gulp'),
+	gutil = require('gulp-util'), // Funciones de utilidad para gulp
+	merge = require('merge2'),
+	ts = require('gulp-typescript'),
+	uglify = require('gulp-uglify'); // Minifica archivos
+
+// Rutas
+var paths = {
+	js: './app/release/**/*.js',
+	ts: './app/**/*.ts'
+};
+
+// Opciones
+var tsProject = ts.createProject({
+    declaration: true, // Genera el archivo d.ts correspondiente
+    noExternalResolve: true // No resuelve archivos que no esten en el input
+});
+
+// Tarea por defecto
+gulp.task('scripts', ['minify-js'], function (){
+    var tsResult = gulp.src(paths.ts)
+        .pipe(ts(tsProject));
+ 
+    return merge([
+        tsResult.dts.pipe(gulp.dest('./app/release')), // Archivo de definiciones
+        tsResult.js.pipe(gulp.dest('./app/release')) // Archivos javascript
+    ]);
+});
+
+// Comprime los archivos javascript
+gulp.task('minify-js', function() {
+	gulp.src(paths.js)
+	.pipe(concat('build.js'))
+	.pipe(uglify().on('error', gutil.log))
+	.pipe(gulp.dest('./app/build'))
+});
+
+// Tareas watch
+gulp.task('watch', ['scripts'], function() {
+    gulp.watch(paths.ts, ['scripts']);
+});
+
+// Tareas por defecto
+gulp.task('default', ['watch']);
